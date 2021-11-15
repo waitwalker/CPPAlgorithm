52fd1ab5eea8390896a4b227682424b8d7647d17
waitwalker <waitwalker@163.com>
2021-11-15 20:32:23 +0800

    输出两数之和

--- CPPAlgorithm/1两数之和/main.cpp
+++ CPPAlgorithm/1两数之和/main.cpp
@@ -7,8 +7,14 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    cout <<"请输入两个数:";
+    int a, b;
+    cin>>a>>b;
+    cout<<"输出结果:"<<a+b<<"\n";
     return 0;
 }
